f85a4b86c5aa3a6a10c63c1595cfb93c225ab7a2
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2024-10-06 01:06:55 +0600

    70% doone

--- JS/video.js
+++ JS/video.js
@@ -1,3 +1,14 @@
+//me system
+function getTimeString(time){
+    const hour = parseInt(time/3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond/60)
+    remainingSecond= remainingSecond % 60;
+    return `${hour}h ${minute}m ${remainingSecond}s ago`
+}
+
+
+
 //1: Fetch, Load and Show Categories
 
 
@@ -10,6 +21,16 @@ const loadCategories = () =>{
     .catch((error) => console.log(error))
 }
 
+
+const loadCategoryVideos = (id) => {
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.category))
+    .catch((error) => console.log(error))
+     
+}
+
 // {
 //     "category_id": "1001",
 //     "category": "Music"
@@ -20,19 +41,19 @@ const displayCategories = (categories) =>{
 
     categories.forEach((item) => {
         console.log(item);
-        //create a button
-        const button = document.createElement('button')
-        button.classList = 'btn';
-        button.innerText = item.category;
 
+        //create a button
+        const buttonContainer = document.createElement("div")
+        buttonContainer.innerHTML=
+        `
+        <button onclick="loadCategoryVideos(${item.category_id})" class="btn">${item.category}</button>
+        `
         //add button to categoryContainer
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
     })
       
 }
 loadCategories();
-
-
 
 
 
@@ -47,30 +68,40 @@ const loadVideos = () =>{
     .catch((error) => console.log(error))
 }
 
-const cardDemo = {
-    "category_id": "1003",
-    "video_id": "aaac",
-    "thumbnail": "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg",
-    "title": "Laugh at My Pain",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/XVHM7NP/kevin.jpg",
-            "profile_name": "Kevin Hart",
-            "verified": false
-        }
-    ],
-    "others": {
-        "views": "1.1K",
-        "posted_date": "13885"
-    },
-    "description": "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
-}
+// const cardDemo = {
+//     "category_id": "1003",
+//     "video_id": "aaac",
+//     "thumbnail": "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg",
+//     "title": "Laugh at My Pain",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/XVHM7NP/kevin.jpg",
+//             "profile_name": "Kevin Hart",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "1.1K",
+//         "posted_date": "13885"
+//     },
+//     "description": "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
+// }
 //create Display Videos
+
+
 const displayVideos = (videos) =>{
     const videoContainer = document.getElementById('videos');
+    videoContainer.innerHTML= "";
+
+    if(videos.length === 0){
+        videoContainer.innerHTML = "No content"
+        return;
+    }
+    
     videos.forEach((video) => {
         console.log(video);
         const card = document.createElement("div");
+        console.log(card);
         card.classList = "card card-compact"
         card.innerHTML=
      `
@@ -82,7 +113,7 @@ const displayVideos = (videos) =>{
        ${
         video.others.posted_date?.length === 0
         ? ""
-         : `<span class="absolute text-white bg-black rounded-md p-2 right-2 bottom-2 ">${video.others.posted_date}</span>`
+         : `<span class="absolute text-white text-xs bg-black rounded-md p-2 right-2 bottom-2 ">${getTimeString(video.others.posted_date)}</span>`
         }
     </figure>
     <div class="px-0 py-3 flex gap-2">
